5aff8b996b6da9b7bfe40301668a8d6c1d7c1dbc
Gabriel Souza Campos <gabriel.s.campos@hotmail.com>
2025-07-09 22:27:56 -0300

    initial backend setup and database configuration.

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { env } from './env.ts';
+import { getRoomsRoute } from './http/routes/get-rooms.ts';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.get("/health", () => {
+  return "OK";
+});
+
+app.register(fastifyCors, { origin: 'localhost:5173' });
+app.register(getRoomsRoute);
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.listen({ port: env.PORT });
