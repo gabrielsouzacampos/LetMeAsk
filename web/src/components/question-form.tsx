1419035f91bf03c7c88abce86e57506093721ac2
Gabriel Souza Campos <gabriel.s.campos@hotmail.com>
2025-07-20 18:38:10 -0300

    feat: add audio recording page and implement room details page functionality.

--- web/src/components/question-form.tsx
+++ web/src/components/question-form.tsx
@@ -18,6 +18,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Textarea } from '@/components/ui/textarea';
+import { useCreateQuestion } from '@/http/use-create-question';
 
 const createQuestionSchema = z.object({
   question: z
@@ -34,6 +35,8 @@ interface QuestionFormProps {
 }
 
 export function QuestionForm({ roomId }: QuestionFormProps) {
+  const { mutateAsync: createQuestion } = useCreateQuestion(roomId);
+
   const form = useForm<CreateQuestionFormData>({
     resolver: zodResolver(createQuestionSchema),
     defaultValues: {
@@ -41,10 +44,11 @@ export function QuestionForm({ roomId }: QuestionFormProps) {
     },
   });
 
-  function handleCreateQuestion(data: CreateQuestionFormData) {
-    // biome-ignore lint/suspicious/noConsole: dev
-    console.log(data, roomId);
+  async function handleCreateQuestion(data: CreateQuestionFormData) {
+    await createQuestion(data);
   }
+
+  const { isSubmitting } = form.formState;
 
   return (
     <Card>
@@ -69,6 +73,7 @@ export function QuestionForm({ roomId }: QuestionFormProps) {
                   <FormControl>
                     <Textarea
                       className="min-h-[100px]"
+                      disabled={isSubmitting}
                       placeholder="O que você gostaria de saber?"
                       {...field}
                     />
@@ -78,7 +83,9 @@ export function QuestionForm({ roomId }: QuestionFormProps) {
               )}
             />
 
-            <Button type="submit">Enviar pergunta</Button>
+            <Button disabled={isSubmitting} type="submit">
+              Enviar pergunta
+            </Button>
           </form>
         </Form>
       </CardContent>
